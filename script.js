bff8c2531a68468749cac3ba3b79a2c3ace19637
hisiegotit <hisie.work@gmail.com>
2024-01-21 11:22:39 +0700

    no  script

--- script.js
+++
@@ -1,9 +0,0 @@
-let myImage = document.querySelector(".card")[0];
-
-myImage.addEventListener("touch", function () {
-  myImage.classList.add("cardHover cardHoverBefore");
-});
-
-myImage.addEventListener("touchEnd", function () {
-  myImage.classList.remove("cardHover cardHoverBefore");
-});
